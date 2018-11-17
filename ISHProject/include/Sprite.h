70562497fbd55264ac16d724bf672689f558a6ef
Kyle <kfalicov@gmail.com>
2018-11-17 17:03:30 -0500

    AssetHandler/Sprite

---
+++ ISHProject/include/Sprite.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SDL.h>
+#include <vector>
+
+
+class Sprite {
+public:
+	Sprite();
+	Sprite(SDL_Surface* spriteSheet, SDL_Rect srcRect);
+	Sprite(SDL_Surface* spriteSheet, std::vector<SDL_Rect> frames);
+	~Sprite();
+
+	void setRectsFromIndices(const int width, const int height, const std::vector<int> indices);
+
+	SDL_Surface* spriteSheet;
+	std::vector<SDL_Rect> frames;
+	int currentFrameIndex;
+	//SDL_Rect srcRect;
+};
---
+++ ISHProject/src/Sprite.cpp
@@ -0,0 +1,46 @@
+#include "Sprite.h"
+
+Sprite::Sprite() {
+}
+
+Sprite::Sprite(SDL_Surface* spriteSheet, SDL_Rect srcRect)
+{
+	this->spriteSheet = spriteSheet;
+	frames = std::vector<SDL_Rect>();
+	frames.push_back(srcRect);
+	currentFrameIndex = 0;
+	//this->srcRect = srcRect;
+}
+
+Sprite::Sprite(SDL_Surface * spriteSheet, std::vector<SDL_Rect> frames)
+{
+	this->spriteSheet = spriteSheet;
+	this->frames = frames;
+	currentFrameIndex = 0;
+}
+
+Sprite::~Sprite() {
+
+}
+
+//width and height of the sprite, indices are zero-based.
+void Sprite::setRectsFromIndices(const int width, const int height, const std::vector<int> indices)
+{
+	if (spriteSheet == nullptr) return;
+
+	std::vector<SDL_Rect> frameRects;
+
+	for (int i = 0; i < indices.size(); i++) {
+		SDL_Rect r = SDL_Rect();
+		r.w = width;
+		r.h = height;
+		r.x = (width * indices[i]) % spriteSheet->w;
+		int indexY = (width * indices[i]) / spriteSheet->w;
+		r.y = indexY * height;
+		frameRects.push_back(r);
+	}
+
+	frames = frameRects;
+
+	//AssetHandler::Instance()->subscribeSprite(this); TODO
+}
